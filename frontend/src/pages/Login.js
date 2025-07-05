e7668865256b8575a6811100236e6482cb44fcfa
Aswi-reddy <sumanaswireddy@gmail.com>
2025-07-05 20:58:17 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `${APIUrl}/auth/login`;
+            const url = `https://expense-tracker-api-amber.vercel.app//auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
