0f6ffdf85d7bec916434ce0d5ab395df80ce7b46
Aswi-reddy <sumanaswireddy@gmail.com>
2025-07-05 21:00:48 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `https://expense-tracker-api-amber.vercel.app//auth/signup`;
+            const url = `https://expense-tracker-api-amber.vercel.app/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
