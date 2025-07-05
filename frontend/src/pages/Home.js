3eb364de8766e41ff2cf07223b1622595378862f
Aswi-reddy <sumanaswireddy@gmail.com>
2025-07-05 21:00:20 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = `${APIUrl}/products`;
+            const url = `https://expense-tracker-api-amber.vercel.app/products`;
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
